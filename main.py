7cd8918da1308faaf14a1060185160c63c28746c
pajilaya2281488 <savvaolevskij@gmail.com>
2023-06-08 14:45:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
-print("test")
-print('ara')
-print('tt')
+class Person:
+    def __init__(self, name):
+        self.name = name
+        self.dog = None
+
+    def adopt_dog(self, dog):
+        self.dog = dog
+        print(f"{self.name} adopted a dog named {dog.name}.")
+
+    def walk_dog(self):
+        if self.dog is not None:
+            print(f"{self.name} is walking with {self.dog.name}.")
+        else:
+            print(f"{self.name} doesn't have a dog to walk.")
+
+    def feed_dog(self):
+        if self.dog is not None:
+            print(f"{self.name} is feeding {self.dog.name}.")
+        else:
+            print(f"{self.name} doesn't have a dog to feed.")
+
+
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print(f"{self.name} says: Woof!")
+
+person = Person("John")
+dog = Dog("Max", "Labrador")
+
+person.adopt_dog(dog)
+person.walk_dog()
+person.feed_dog()
+dog.bark()
