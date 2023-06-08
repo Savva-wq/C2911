f8be921c771c7131983398c9a1d8a2aed7722e89
pajilaya2281488 <savvaolevskij@gmail.com>
2023-06-08 14:50:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-print("test")
-print('ara')
-print('tt')
+import random
+
+class Encryptor:
+    def __init__(self, number):
+        self.number = number
+
+    def __str__(self):
+        encrypted_number = self._perform_operation()
+        return f"Encrypted number: {encrypted_number}"
+
+    def _perform_operation(self):
+        operation = random.choice(['+', '-', '*', '/'])
+        operand = random.randint(1, 10)
+
+        if operation == '+':
+            return self.number + operand
+        elif operation == '-':
+            return self.number - operand
+        elif operation == '*':
+            return self.number * operand
+        elif operation == '/':
+            return self.number / operand
+
+encryptor = Encryptor(42)
+
+print(encryptor)
